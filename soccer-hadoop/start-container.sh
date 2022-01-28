1297df5e3b3211cf3db2e7890e08e31037d49561
artiumrexbellator <ait-addi@hotmail.com>
2022-01-28 03:24:26 +0100

    hadoop fix port binding

--- soccer-hadoop/start-container.sh
+++ soccer-hadoop/start-container.sh
@@ -9,7 +9,7 @@ sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
                 --net=hadoop \
-                -p 50070:50070 \
+                -p 9870:9870 \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
