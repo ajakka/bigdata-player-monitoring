2d005b628eabef844ced35f751ff45e3fbf6441c
artiumrexbellator <ait-addi@hotmail.com>
2022-01-28 15:38:55 +0100

    fix the built of docker hadoop && added networks to containers

--- soccer-hadoop/start-container.sh
+++ soccer-hadoop/start-container.sh
@@ -8,7 +8,7 @@ N=${1:-3}
 sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
-                --net=hadoop \
+                --net=bigdata-player-monitoring_default \
                 -p 9870:9870 \
                 -p 8088:8088 \
                 --name hadoop-master \
@@ -23,7 +23,7 @@ do
 	sudo docker rm -f hadoop-slave$i &> /dev/null
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
-	                --net=hadoop \
+	                --net=bigdata-player-monitoring_default \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
 	                soccer/hadoop &> /dev/null
