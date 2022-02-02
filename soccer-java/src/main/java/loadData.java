401052efa28e7ddf53eee1de7a53fcc8fd460576
artiumrexbellator <ait-addi@hotmail.com>
2022-02-02 23:29:22 +0100

    test rdd

--- soccer-java/src/main/java/loadData.java
+++ soccer-java/src/main/java/loadData.java
@@ -1,10 +1,15 @@
-import org.apache.spark.SparkContext;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
+import java.util.List;
+import java.lang.System;
+
 public class loadData {
     public static void main(String[] args) {
         SparkConf sc=new SparkConf().setMaster("spark://localhost:7077").setAppName("Test");
-        SparkContext context=new SparkContext(sc);
-
+        JavaSparkContext context=new JavaSparkContext(sc);
+        JavaRDD<String>data=context.textFile("hdfs://172.22.0.2:9000/user/root/data/players-madrid.csv");
+        System.out.println(data.count());
     }
 }
