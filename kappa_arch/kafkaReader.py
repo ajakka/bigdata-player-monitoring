31b22fc1878273ae1758353156db896c0a50273f
artiumrexbellator <ait-addi@hotmail.com>
2022-03-30 19:23:54 +0000

    saveToHDFS

--- kappa_arch/kafkaReader.py
+++ kappa_arch/kafkaReader.py
@@ -16,6 +16,9 @@ class KafkaReader:
     #returns array of dicts,each dict contains id of the player as str and the heartBeat
     def getData(self,folderPath):
         heartBeats=[]
+        spark=Spark()
+        session=spark.getSession("saveToHDFS")
+        session.conf.set("spark.sql.execution.arrow.enabled","true")
         df=KafkaReader().readFromKafka()
         df=df.withColumn('key_str', df['key'].cast('string').alias('key_str')).drop(
         'key').withColumn('value_str', df['value'].cast('string').alias('value_str')).drop('value')
@@ -25,8 +28,10 @@ class KafkaReader:
             row=json.loads(record)
             heartBeats.append([list(row.keys())[0],list(row.values())[0]])
         df=pd.DataFrame(heartBeats,columns=['id','hb'])
-        df=df.groupby('id').agg([pd.np.mean])
-        df.to_csv(path_or_buf=f"{folderPath}/{datetime.today().strftime('%Y-%m-%d')}.csv", sep=',')
+        df=df.groupby('id',as_index=False).agg(lambda x:pd.np.mean(x))
+        hdfsDF=session.createDataFrame(df)
+        hdfsDF.write.mode("overwrite").option("header",'true').csv(f"hdfs://172.22.0.2:9000/user/root/data/{datetime.today().strftime('%Y-%m-%d')}.csv")
+        #df.to_csv(path_or_buf=f"{folderPath}/{datetime.today().strftime('%Y-%m-%d')}.csv", sep=',')
     
 kr=KafkaReader()
 kr.getData("/home/mustapha/bigdata-player-monitoring/kappa_arch/data")
